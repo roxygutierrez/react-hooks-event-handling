83f01625b1e861fba38d91a8547686e6b336e664
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-02-07 16:03:56 -0600

    reviewed

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+  // return <button onClick={() => console.log("Teehee!")}> Tickle me!</button>;
+
+  function tickle(event) {
+    console.log(event);
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
